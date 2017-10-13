3b1d09f6802bcc5a935c702e983ce47c54c1201d
Theo C <DyingEcho@users.noreply.github.com>
2017-10-13 21:06:28 +0200

    Create game.py

---
+++ game.py
@@ -0,0 +1,232 @@
+# adventure by @DyingEcho
+# Copyright ©2017 @DyingEcho. Some rights reserved.
+# Licensed under the MIT License.
+from sys import exit
+from time import sleep as wait
+import pickle
+from random import randrange
+from os.path import expanduser
+
+
+###########
+#  SETUP  #
+###########
+print()
+print()
+xMax = 7  # maximum X length
+yMax = 7  # maximum Y length
+playerX = int(round(xMax / 2))  # as close as possible to the middle of the areaMap
+playerY = int(round(yMax / 2))  # as close as possible to the middle of the areaMap
+areaMap = []
+time = -1  # hours passed so goes up to 24. incremented at start of gameplay, starts at 0.
+day = 0  # we will increment this at the very start of gameplay so's not to confuse the user
+inventory = []
+maxInventory = 50  # how many items can the player have?
+cheats = False
+health = 1.0
+hunger = 1.0
+
+xSampleData = []  # this will be cloned on the X axis for every Y-line
+for i in range(0, xMax):
+	xSampleData.append("_")  # fill it with None for now, we will generate a areaMap later
+for i in range(0, yMax):
+	areaMap.append(xSampleData)  # fill up the areaMap with Nones
+
+
+
+class baseAnimal:
+	def __init__(self, animalID):
+		self.animalName = "Herobrine"  # will be replaced in all subclasses (probably with 99.9% chance to freak people out)
+		self.health = 999999  # unkillable
+		# TODO: Replace "Herobrine" with "The Lord Maxi"
+		self.animalID = animalID  # to be replaced. will be based on biome, so there will be multiple ID 1s, 2s, etc.
+		self.deadliness = 100  # percentage
+
+	def __str__(self):
+		return self.animalName
+
+
+
+class pigAnimal(baseAnimal):
+	def __init__(self, animalID):
+		self.animalName = "Pig"
+		self.health = 4
+		self.animalID = animalID
+		self.deadliness = 2
+
+
+
+class baseBiome:
+	def __init__(self):
+		self.biomeName = "Void"  # will be replaced in all subclasses
+		self.mineralName = "VoidMineral"  # will be replaced
+		self.mineralQuant = 0  # will be replaced
+		self.animals = []  # will be replaced
+
+	def __str__(self):
+		return self.biomeType
+
+
+class woodsBiome(baseBiome):
+	def __init__(self):
+		self.mineralName = "Trees"
+		self.mineralQuant = randrange(7, 18)
+
+		# generate animals
+		self.animals =
+
+
+
+def die(cause, killer="animal"):
+	if cause == "suicide":
+		print("You jump off a cliff.")
+
+	elif cause == "animal":
+		print("Your attempt to find food was rudely interupted by a gang of " + killer + "s.")
+		print("Aware of their cousin's distress, they attack you. They come in such numbers that you are helpless.")
+
+	print("Goodbye, cruel world.")
+	e = exit()
+
+
+def formTempBackup():
+	return (time, day, areaMap, playerX, playerY, inventory, cheats)
+
+
+def loadTempBackup(backup):
+	global time
+	global day
+	global areaMap
+	global playerX
+	global playerY
+	global inventory
+	global cheats
+	time = backup[0]
+	day = backup[1]
+	areaMap = backup[2]
+	inventory = backup[3]
+	cheats = backup[4]
+
+
+def loadGame():
+	global time
+	global day
+	global areaMap
+	global playerX
+	global playerY
+	global inventory
+	global cheats
+	loadFailBackup = formTempBackup()
+	try:
+		loadedData = pickle.load(open(expanduser("~/adventure.adgf"), "rb+"))
+		time = loadedData[0]
+		day = loadedData[1]
+		areaMap = loadedData[2]
+		playerX = loadedData[3]
+		playerY = loadedData[4]
+		inventory = loadedData[5]
+		cheats = loadedData[6]
+
+		assert isinstance(time, int)
+		assert isinstance(day, int)
+		assert isinstance(areaMap, list)
+		assert isinstance(inventory, list)
+		assert isinstance(cheats, bool)
+
+	except (pickle.UnpicklingError, EOFError):  # anything could fail!
+		print("Load failed - save file may be corrupt. Sorry about that.")
+		print("Reloading pre-load state from backup...")
+		loadTempBackup(loadFailBackup)
+
+	except (AssertionError, IndexError):
+		print("Load failed - save file may be from an older version of this game. Sorry about that.")
+		print("Reloading pre-load state from backup...")
+		loadTempBackup(loadFailBackup)
+
+	except FileNotFoundError:
+		print("Load failed - save file does not exist. Sorry about that.")
+		print("Reloading pre-load state from backup...")
+		loadTempBackup(loadFailBackup)
+
+
+
+
+while True:
+	###################
+	#  GETTING READY  #
+	###################
+	time += 1
+	if time > 23:
+		time = 0  # hours reset every day
+		day += 1
+
+	# print areaMap
+	print("MAP:")
+	for yLine in areaMap:
+		print(str(yLine).replace("[", "").replace("]", "").replace("'", "").replace(", ", ""))
+
+	print()
+
+	# print player position
+	print("Your position: X" + str(playerX) + " Y" + str(playerY))
+
+	# print time/day
+	print("The time:      " + str(time) + "hrs")
+	print("Day:           " + str(day))
+
+
+
+	##############
+	#  GAMEPLAY  #
+	##############
+	action = input(">")
+	action = action.lower().strip(" ")  # lower and strip of whitespace
+
+	if action == "exit":
+		die("suicide")
+	elif action == "help":
+		print("exit")
+		print("	kill yourself and exit the game")
+		print("wait")
+		print("	do nothing and stay where you are")
+		print("go north")
+		print("	head north (positive Y)")
+		print("go south")
+		print("	head south (negative Y)")
+		print("go east")
+		print("	head east (positive X)")
+		print("go west")
+		print("	head west (negative X)")
+	elif action == "wait":
+		pass
+	elif action == "go north":
+		if playerY >= yMax:
+			print("Can't go north from here!")
+		else:
+			playerY += 1
+	elif action == "go south":
+		if playerY <= 1:
+			print("Can't go south from here!")
+		else:
+			playerY -= 1
+	elif action == "go east":
+		if playerX >= xMax:
+			print("Can't go east from here!")
+		else:
+			playerX += 1
+	elif action == "go west":
+		if playerX <= 1:
+			print("Can't go south from here!")
+		else:
+			playerX -= 1
+	elif action == "save":
+		pickle.dump((time, day, areaMap, playerX, playerY, inventory, cheats), open(expanduser("~/adventure.adgf"), "wb+"))
+		print("Saved gamefile at ~/adventure.adgf - DO NOT RENAME! You can only save one gamefile!")
+	elif action == "load":
+		loadGame()
+	else:
+		print("Didn't understand. Try 'help' for a list of commands.")
+
+	wait(2)
+	print()
+	print()
